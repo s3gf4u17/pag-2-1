ca36ab539346746ed31cf402bf084f223ed9d9d7
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-12-09 02:43:56 +0100

    communicate with the server

--- app/script.js
+++ app/script.js
@@ -1,40 +1,50 @@
-markers = []
+var markers = [];
+var layer = null;
 
-function setPointCoordinates(event,map,id) {
+function findPath(map) {
+    // fetch path geojson
+    const settings = {
+        method:"POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({
+            lat0:markers[0]._latlng.lat,
+            lon0:markers[0]._latlng.lng,
+            lat1:markers[1]._latlng.lat,
+            lon1:markers[1]._latlng.lng
+        })
+    };
+    // const geojson = fetch("http://127.0.0.1:8000/test_line/",settings)
+    const geojson = fetch("http://127.0.0.1:8000/test_point/",settings)
+    .then(response=>response.json())
+    .then(geojson=>{
+        console.log(geojson);
+        if (layer!=null) map.removeLayer(layer);
+        layer = L.geoJSON(geojson,{style:{
+            radius:5,
+            opacity:1.2,
+            color:'rgba(0,0,0,1.0)',
+            weight: 5.8
+        }});
+        map.addLayer(layer);
+    });
+}
+
+function setMarker(event,map,id) {
+    // delete old marker associated with 'id'
     new_markers = []
     markers.forEach(function(marker) {
         if (marker._id==id) map.removeLayer(marker);
         else new_markers.push(marker);
     })
-    markers = new_markers
+    // create new marker in its place
+    markers=new_markers
     var marker = L.marker(event.latlng);
-    marker._id = id
-    test = map.addLayer(marker);
-    marker._icon.classList.add("huechange");
+    marker._id = id;
+    handle = map.addLayer(marker);
+    if (id=="left-click") marker._icon.classList.add("huechange");
     markers.push(marker);
-}
-
-// function(e) {
-//     var location = e.latlng;
-//     var marker = L.marker(location).addTo(map);
-//     marker._icon.classList.add("huechange");
-//     // var popup = L.popup().setLatLng(location).setContent('<p>A</p>').openOn(map);
-//     console.log("left click: ",location);
-// }
-
-function fetchVectorLayer(LC,input_layer,name){
-    var color = getComputedStyle(document.documentElement).getPropertyValue('--accent');
-    var layer = L.geoJSON(input_layer,{
-        // onEachFeature:oEF,
-        // pointToLayer:PToL
-        style: {
-            radius:5,
-            opacity:0.2,
-            color:'rgba(0,0,0,1.0)',
-            weight:0.8
-        }
-    });
-    LC.addOverlay(layer,name);
+    console.log(marker);
+    if (markers.length==2) findPath(map);
 }
 
 L.Control.Test = L.Control.extend({
@@ -56,19 +66,14 @@ L.Control.Test = L.Control.extend({
 });
 
 function renderApp() {
-    var map = L.map('map',{maxZoom:14,"zoomControl":false}).setView([53.0138,18.5984],10);
+    var map = L.map('map',{maxZoom:14,"zoomControl":false}).setView([53.0138,18.5984],12);
     var background = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',{maxZoom:50,attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'});
     background.addTo(map);
     var test = new L.Control.Test("12",{position:"topleft"});
     L.control.scale({"position":"bottomleft"}).addTo(map);
     L.control.zoom({"position":"bottomleft"}).addTo(map);
-    map.on('click',function (e) {setPointCoordinates(e,map,'left-click')});
-    map.on('contextmenu',function(e) {
-        var location = e.latlng;
-        var marker = L.marker(location).addTo(map);
-        // var popup = L.popup().setLatLng(location).setContent('<p>B</p>').openOn(map);
-        console.log("right click: ",location);
-    });
+    map.on('click',function (e) {setMarker(e,map,'left-click')});
+    map.on('contextmenu',function(e) {setMarker(e,map,'right-click')});
     test.addTo(map);
 }
 
